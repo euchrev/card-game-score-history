b0d450e50b9a192897f43f47300494fd7ea6ecdc
RoryLetteney <rory.letteney@gmail.com>
2019-05-06 12:12:19 -0500

    File structure and basic app setup.

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, `App listening on ${PORT}.`);
