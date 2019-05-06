5f9ba91c3c4bb14df2a27967aa5a41d74df8ab92
RoryLetteney <rory.letteney@gmail.com>
2019-05-06 12:21:41 -0500

    App is live.

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, `App listening on ${PORT}.`);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.listen(PORT, console.log(`App listening on ${PORT}.`));
