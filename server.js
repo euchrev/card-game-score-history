c1b56313b214e083c0425b00b878af027b3b1fdb
Michele Saba <michelemsaba@gmail.com>
2019-05-07 17:07:22 -0500

    added back the route for about page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const pg = require('pg');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const express = require('express');
-const methodOverride = require('method-override');
+const pg = require("pg");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const express = require("express");
+const methodOverride = require("method-override");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,9 +16,9 @@ const creds = require("./client_secret.json");
 const async = require("async");
 const SECRET_KEY_TEST = process.env.SECRET_KEY_TEST;
 const PUBLISHABLE_KEY_TEST = process.env.PUBLISHABLE_KEY_TEST;
-const stripe = require('stripe')(SECRET_KEY_TEST);
+const stripe = require("stripe")(SECRET_KEY_TEST);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(
   express.urlencoded({
@@ -27,7 +27,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on("err", err => console.log(err));
@@ -39,7 +39,7 @@ async.series(
   [
     function setAuth(step) {
       console.log("set auth");
-      
+
       doc.useServiceAccountAuth(creds, step);
     },
 
@@ -77,7 +77,7 @@ async.series(
   }
 );
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(
   express.urlencoded({
@@ -85,104 +85,111 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/dashboard', (req, res) => console.log(req.cookies.auth));
-app.get('/groups', (req, res) => loginGroup(req.body, res));
-app.get('/payment', (req, res) => stripePayment(req, res));
+app.get("/", (req, res) => res.render("pages/index"));
+app.get("/login", (req, res) => res.render("pages/login"));
+app.get("/about", (req, res) => res.render("pages/about"));
+app.get("/dashboard", (req, res) => console.log(req.cookies.auth));
+app.get("/groups", (req, res) => loginGroup(req.body, res));
+app.get("/payment", (req, res) => stripePayment(req, res));
 app.get(
-  '/logout',
-  (req, res) => res.clearCookie('auth') && res.redirect('/login')
+  "/logout",
+  (req, res) => res.clearCookie("auth") && res.redirect("/login")
 );
 
-app.post('/groups', (req, res) => createGroup(req.body, res));
-app.post('/members', (req, res) => addMember(req, res));
+app.post("/groups", (req, res) => createGroup(req.body, res));
+app.post("/members", (req, res) => addMember(req, res));
 
-app.put('/members', (req, res) => updateMember(req, res));
+app.put("/members", (req, res) => updateMember(req, res));
 
-app.delete('/members', (req, res) => deleteMember(req, res));
+app.delete("/members", (req, res) => deleteMember(req, res));
 
 function stripePayment(req, res) {
   (async () => {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        name: 'EuchreV Subscription',
-        description: 'Lifetime subscription of EuchreV',
-        images: ['https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Euchre.jpg/220px-Euchre.jpg'],
-        amount: 1000,
-        currency: 'usd',
-        quantity: 1
-      }],
-      success_url: 'https://localhost:3000/dashboard',
-      cancel_url: 'https://localhost:3000/'
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          name: "EuchreV Subscription",
+          description: "Lifetime subscription of EuchreV",
+          images: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Euchre.jpg/220px-Euchre.jpg"
+          ],
+          amount: 1000,
+          currency: "usd",
+          quantity: 1
+        }
+      ],
+      success_url: "https://localhost:3000/dashboard",
+      cancel_url: "https://localhost:3000/"
     });
-    res.render('pages/payment.ejs', {
+    res.render("pages/payment.ejs", {
       sessionId: session.id
-    })
+    });
   })();
 }
 
 const lookupGroup = handler => {
-  const SQL = 'SELECT * FROM groups WHERE name=$1';
+  const SQL = "SELECT * FROM groups WHERE name=$1";
   const values = [handler.query.name];
   return client
     .query(SQL, values)
     .then(results =>
-      !results.rows.length ?
-      handler.cacheMiss(results) :
-      handler.cacheHit(results)
+      !results.rows.length
+        ? handler.cacheMiss(results)
+        : handler.cacheHit(results)
     );
 };
 
 const lookupMember = handler => {
-  const SQL = 'SELECT * FROM group_members WHERE name=$1 AND group_id=$2';
-  const values = [`${handler.query.firstname} ${handler.query.lastname}`, handler.query.groupID];
+  const SQL = "SELECT * FROM group_members WHERE name=$1 AND group_id=$2";
+  const values = [
+    `${handler.query.firstname} ${handler.query.lastname}`,
+    handler.query.groupID
+  ];
   return client
     .query(SQL, values)
     .then(results =>
-      !results.rows.length ?
-      handler.cacheMiss(results) :
-      handler.cacheHit(results)
+      !results.rows.length
+        ? handler.cacheMiss(results)
+        : handler.cacheHit(results)
     );
 };
 
 function Group(info) {
   (this.name = info.name),
-  (this.email = info.email),
-  (this.password = info.password),
-  (this.paid = info.paid);
+    (this.email = info.email),
+    (this.password = info.password),
+    (this.paid = info.paid);
 }
 
 function Member(info) {
-  (this.name = info.name),
-  (this.groupID = info.groupID);
+  (this.name = info.name), (this.groupID = info.groupID);
 }
 
-Group.prototype.save = function () {
+Group.prototype.save = function() {
   const SQL =
-    'INSERT INTO groups (name, email, password, paid) VALUES($1,$2,$3,$4) RETURNING id';
+    "INSERT INTO groups (name, email, password, paid) VALUES($1,$2,$3,$4) RETURNING id";
   const values = [this.name, this.email, this.password, this.paid];
   return client.query(SQL, values);
 };
 
-Member.prototype.save = function () {
-  const SQL = 'INSERT INTO group_members (name, group_id) VALUES($1,$2)';
+Member.prototype.save = function() {
+  const SQL = "INSERT INTO group_members (name, group_id) VALUES($1,$2)";
   const values = [this.name, this.groupID];
   return client.query(SQL, values);
-}
+};
 
-Member.update = function (info) {
-  const SQL = 'UPDATE group_members SET name=$1 WHERE name=$2 AND group_id=$3';
+Member.update = function(info) {
+  const SQL = "UPDATE group_members SET name=$1 WHERE name=$2 AND group_id=$3";
   const values = [info.newName, info.currentName, info.groupID];
   return client.query(SQL, values);
-}
+};
 
-Member.delete = function (info) {
-  const SQL = 'DELETE FROM group_members WHERE name=$1 AND group_id=$2';
+Member.delete = function(info) {
+  const SQL = "DELETE FROM group_members WHERE name=$1 AND group_id=$2";
   const values = [info.name, info.groupID];
   return client.query(SQL, values);
-}
+};
 
 const createGroup = (req, res) => {
   const validation = [
@@ -194,7 +201,7 @@ const createGroup = (req, res) => {
   const handler = {
     query: req,
     cacheHit: result => {
-      res.send('Cache hit');
+      res.send("Cache hit");
     },
     cacheMiss: result => {
       const hashedPassword = bcrypt.hashSync(req.password, 8);
@@ -207,14 +214,15 @@ const createGroup = (req, res) => {
       if (validation.every(result => result === true)) {
         const newGroup = new Group(groupInfo);
         newGroup.save().then(result => {
-          const token = jwt.sign({
+          const token = jwt.sign(
+            {
               id: result.rows[0].id
             },
             SECRET
           );
-          res.clearCookie('auth');
-          res.cookie('auth', token);
-          res.redirect('/dashboard');
+          res.clearCookie("auth");
+          res.cookie("auth", token);
+          res.redirect("/dashboard");
         });
       }
     }
@@ -232,20 +240,21 @@ const loginGroup = (req, res) => {
         result.rows[0].password
       );
       if (passwordIsValid) {
-        const token = jwt.sign({
+        const token = jwt.sign(
+          {
             id: result.rows[0].id
           },
           SECRET
         );
-        res.clearCookie('auth');
-        res.cookie('auth', token);
-        res.redirect('/dashboard');
+        res.clearCookie("auth");
+        res.cookie("auth", token);
+        res.redirect("/dashboard");
       } else {
         handler.cacheMiss();
       }
     },
     cacheMiss: result => {
-      res.send('Cache miss');
+      res.send("Cache miss");
     }
   };
 
@@ -253,7 +262,11 @@ const loginGroup = (req, res) => {
 };
 
 const addMember = (req, res) => {
-  const groupID = jwt.verify(req.cookies.auth, SECRET, (err, decoded) => decoded.id);
+  const groupID = jwt.verify(
+    req.cookies.auth,
+    SECRET,
+    (err, decoded) => decoded.id
+  );
   const firstname = req.body.firstname;
   const lastname = req.body.lastname;
   const handler = {
@@ -263,23 +276,26 @@ const addMember = (req, res) => {
       groupID
     },
     cacheHit: result => {
-      console.log('Member exists');
+      console.log("Member exists");
     },
     cacheMiss: result => {
       const newMember = new Member({
         name: `${req.body.firstname} ${req.body.lastname}`,
         groupID
       });
-      newMember.save()
-        .then(result => res.redirect('/members'));
+      newMember.save().then(result => res.redirect("/members"));
     }
-  }
+  };
 
   lookupMember(handler);
-}
+};
 
 const updateMember = (req, res) => {
-  const groupID = jwt.verify(req.cookies.auth, SECRET, (err, decoded) => decoded.id);
+  const groupID = jwt.verify(
+    req.cookies.auth,
+    SECRET,
+    (err, decoded) => decoded.id
+  );
   const firstname = req.body.firstname;
   const lastname = req.body.lastname;
   const currentName = req.body.currentname;
@@ -294,20 +310,23 @@ const updateMember = (req, res) => {
         newName: `${firstname} ${lastname}`,
         currentName,
         groupID
-      }
-      Member.update(memberInfo)
-        .then(result => res.redirect('/members'));
+      };
+      Member.update(memberInfo).then(result => res.redirect("/members"));
     },
     cacheMiss: result => {
-      console.log('Member does not exist');
+      console.log("Member does not exist");
     }
-  }
+  };
 
   lookupMember(handler);
-}
+};
 
 const deleteMember = (req, res) => {
-  const groupID = jwt.verify(req.cookies.auth, SECRET, (err, decoded) => decoded.id);
+  const groupID = jwt.verify(
+    req.cookies.auth,
+    SECRET,
+    (err, decoded) => decoded.id
+  );
   const name = `${req.body.firstname} ${req.body.lastname}`;
   const handler = {
     query: {
@@ -315,15 +334,14 @@ const deleteMember = (req, res) => {
       name
     },
     cacheHit: result => {
-      Member.delete(handler.query)
-        .then(result => res.redirect('/members'));
+      Member.delete(handler.query).then(result => res.redirect("/members"));
     },
     cacheMiss: result => {
-      console.log('Member does not exist');
+      console.log("Member does not exist");
     }
-  }
+  };
 
   lookupMember(handler);
-}
+};
 
 app.listen(PORT, console.log(`App listening on ${PORT}.`));
